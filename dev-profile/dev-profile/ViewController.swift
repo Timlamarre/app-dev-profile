7490b47f466b79ae6661d1765a38143ae6fdf6b2
Timothée Lamarre <timlamarre@gmail.com>
2018-07-04 12:18:25 +0200

    initial xcode project

---
+++ dev-profile/dev-profile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Timothée Lamarre on 04/07/2018.
+//  Copyright © 2018 Timothée Lamarre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
